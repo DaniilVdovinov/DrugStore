b815a39927775259b768638f66772613a1f3140e
Danilka1337 <daniilvdovinov@MacBook-Pro-Daniil.local>
2019-11-24 10:00:12 +0300

    add services andcontrollers

---
+++ src/main/java/ru/itis/drugstore/models/User.java
@@ -0,0 +1,34 @@
+package ru.itis.drugstore.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "usr")
+public class User{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Enumerated(value = EnumType.STRING)
+    private UserState state;
+
+    private String confirmString;
+
+    public boolean isEnabled() {
+        return this.getState().equals(UserState.CONFIRMED);
+    }
+}
